d8e145dfa6b55b90f4bee460ffc06a6dd2e02f2f
Sergii Permiakov <serg.permyakov@gmail.com>
2025-03-25 19:28:51 +0000

    more refactor

--- artifact/scripts/gamepad.lua
+++ artifact/scripts/gamepad.lua
@@ -50,7 +50,20 @@ InputSessionState = {
     isRShoulderHeadR= false,
     isRShoulderHeadL= false,
     -- State properties
-    gamepadState = nil
+    gamepadState = nil,
+    
+    -- Additional non-gamepad state for holstering system
+    Key1 = false,
+    Key2 = false,
+    Key3 = false,
+    Key4 = false,
+    Key5 = false,
+    Key6 = false,
+    Key7 = false,
+    isDpadUp = false,
+    isDpadDown = false,
+    isDpadLeft = false,
+    isDpadRight = false
 }
 
 -- Constructor
@@ -63,20 +76,75 @@ end
 -- Update state from XInput
 function InputSessionState:updateXInputState(state)
     self.gamepadState = state
-	ThumbLX = self.gamepadState.sThumbLX
-	ThumbLY = self.gamepadState.sThumbLY
-	ThumbRX = self.gamepadState.sThumbRX
-	ThumbRY = self.gamepadState.sThumbRY
-	LTrigger= self.gamepadState.bLeftTrigger
-	RTrigger= self.gamepadState.bRightTrigger
-	rShoulder= isButtonPressed(XINPUT_GAMEPAD_RIGHT_SHOULDER)
-	lShoulder= isButtonPressed(XINPUT_GAMEPAD_LEFT_SHOULDER)
-	lThumb   = isButtonPressed(XINPUT_GAMEPAD_LEFT_THUMB)
-	rThumb   = isButtonPressed(XINPUT_GAMEPAD_RIGHT_THUMB)
-	Abutton  = isButtonPressed(XINPUT_GAMEPAD_A)
-	Bbutton  = isButtonPressed(XINPUT_GAMEPAD_B)
-	Xbutton  = isButtonPressed(XINPUT_GAMEPAD_X)
-	Ybutton  = isButtonPressed(XINPUT_GAMEPAD_Y)
+    self.ThumbLX = state.Gamepad.sThumbLX
+    self.ThumbLY = state.Gamepad.sThumbLY
+    self.ThumbRX = state.Gamepad.sThumbRX
+    self.ThumbRY = state.Gamepad.sThumbRY
+    self.LTrigger = state.Gamepad.bLeftTrigger
+    self.RTrigger = state.Gamepad.bRightTrigger
+    self.rShoulder = self:isButtonPressed(XINPUT_GAMEPAD_RIGHT_SHOULDER)
+    self.lShoulder = self:isButtonPressed(XINPUT_GAMEPAD_LEFT_SHOULDER)
+    self.lThumb = self:isButtonPressed(XINPUT_GAMEPAD_LEFT_THUMB)
+    self.rThumb = self:isButtonPressed(XINPUT_GAMEPAD_RIGHT_THUMB)
+    self.Abutton = self:isButtonPressed(XINPUT_GAMEPAD_A)
+    self.Bbutton = self:isButtonPressed(XINPUT_GAMEPAD_B)
+    self.Xbutton = self:isButtonPressed(XINPUT_GAMEPAD_X)
+    self.Ybutton = self:isButtonPressed(XINPUT_GAMEPAD_Y)
+end
+
+-- Reset key state variables (does not modify gamepad state)
+function InputSessionState:Reset()
+    self.rGrabActive = false
+    self.lGrabActive = false
+    self.LZone = 0
+    self.RZone = 0
+    self.LWeaponZone = 0
+    self.RWeaponZone = 0
+    
+    self.lThumbSwitchState = 0
+    self.lThumbOut = false
+    self.rThumbSwitchState = 0
+    self.rThumbOut = false
+    self.isReloading = false
+    self.ReadyUpTick = 0
+    self.LTriggerWasPressed = 0
+    self.RTriggerWasPressed = 0
+    
+    self.isFlashlightToggle = false
+    self.isButtonA = false
+    self.isButtonB = false
+    self.isButtonX = false
+    self.isButtonY = false
+    self.isRShoulder = false
+    
+    self.isCrouch = false
+    self.isJump = false
+    self.vecy = 0
+    
+    self.Key1 = false
+    self.Key2 = false
+    self.Key3 = false
+    self.Key4 = false
+    self.Key5 = false
+    self.Key6 = false
+    self.Key7 = false
+    self.KeyG = false
+    self.KeyM = false
+    self.KeyF = false
+    self.KeyB = false
+    self.KeyI = false
+    self.KeySpace = false
+    self.KeyCtrl = false
+    
+    self.isDpadUp = false
+    self.isDpadDown = false
+    self.isDpadLeft = false
+    self.isDpadRight = false
+    
+    self.isRShoulderHeadR = false
+    self.isRShoulderHeadL = false
+    
+    -- Don't reset actual gamepad hardware state values
 end
 
 function InputSessionState:isButtonPressed(button)
