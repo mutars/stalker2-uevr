f301e0c3f37dfd048a24ba193f4504fa668faee6
Sergii Permiakov <serg.permyakov@gmail.com>
2025-04-06 05:33:09 +0000

    bug in haptics

--- artifact/scripts/stalker2/haptics.lua
+++ artifact/scripts/stalker2/haptics.lua
@@ -1,32 +1,35 @@
 local BodyZones = require("gestures.bodyzone")
 
-local function createHapticCB(left)
-    return function(gesture, context)
-        if left then
-            local leftController = uevr.params.vr.get_left_joystick_source()
-            uevr.params.vr.trigger_haptic_vibration(0.0, 0.1, 1.0, 100.0, leftController)
-        else
-            local rightController = uevr.params.vr.get_right_joystick_source()
-            uevr.params.vr.trigger_haptic_vibration(0.0, 0.1, 1.0, 100.0, rightController)
-        end
+local function HapticLeftCB(gesture, context)
+    if gesture:JustActivated() then
+        local leftController = uevr.params.vr.get_left_joystick_source()
+        uevr.params.vr.trigger_haptic_vibration(0.0, 0.1, 1.0, 100.0, leftController)
     end
-
 end
 
-BodyZones.headZoneLH:SetExecutionCallback(createHapticCB(true))
-BodyZones.leftShoulderZoneLH:SetExecutionCallback(createHapticCB(true))
-BodyZones.rightShoulderZoneLH:SetExecutionCallback(createHapticCB(true))
-BodyZones.rightHipZoneLH:SetExecutionCallback(createHapticCB(true))
-BodyZones.leftHipZoneLH:SetExecutionCallback(createHapticCB(true))
-BodyZones.leftChestZoneLH:SetExecutionCallback(createHapticCB(true))
-BodyZones.rightChestZoneLH:SetExecutionCallback(createHapticCB(true))
-BodyZones.lowerBackZoneLH:SetExecutionCallback(createHapticCB(true))
 
-BodyZones.headZoneRH:SetExecutionCallback(createHapticCB(false))
-BodyZones.leftShoulderZoneRH:SetExecutionCallback(createHapticCB(false))
-BodyZones.rightShoulderZoneRH:SetExecutionCallback(createHapticCB(false))
-BodyZones.rightHipZoneRH:SetExecutionCallback(createHapticCB(false))
-BodyZones.leftHipZoneRH:SetExecutionCallback(createHapticCB(false))
-BodyZones.leftChestZoneRH:SetExecutionCallback(createHapticCB(false))
-BodyZones.rightChestZoneRH:SetExecutionCallback(createHapticCB(false))
-BodyZones.lowerBackZoneRH:SetExecutionCallback(createHapticCB(false))
+local function HapticRightCB(gesture, context)
+    if gesture:JustActivated() then
+        local rightController = uevr.params.vr.get_right_joystick_source()
+        uevr.params.vr.trigger_haptic_vibration(0.0, 0.1, 1.0, 100.0, rightController)
+    end
+end
+
+
+BodyZones.headZoneLH:SetExecutionCallback(HapticLeftCB)
+BodyZones.leftShoulderZoneLH:SetExecutionCallback(HapticLeftCB)
+BodyZones.rightShoulderZoneLH:SetExecutionCallback(HapticLeftCB)
+BodyZones.rightHipZoneLH:SetExecutionCallback(HapticLeftCB)
+BodyZones.leftHipZoneLH:SetExecutionCallback(HapticLeftCB)
+BodyZones.leftChestZoneLH:SetExecutionCallback(HapticLeftCB)
+BodyZones.rightChestZoneLH:SetExecutionCallback(HapticLeftCB)
+BodyZones.lowerBackZoneLH:SetExecutionCallback(HapticLeftCB)
+
+BodyZones.headZoneRH:SetExecutionCallback(HapticRightCB)
+BodyZones.leftShoulderZoneRH:SetExecutionCallback(HapticRightCB)
+BodyZones.rightShoulderZoneRH:SetExecutionCallback(HapticRightCB)
+BodyZones.rightHipZoneRH:SetExecutionCallback(HapticRightCB)
+BodyZones.leftHipZoneRH:SetExecutionCallback(HapticRightCB)
+BodyZones.leftChestZoneRH:SetExecutionCallback(HapticRightCB)
+BodyZones.rightChestZoneRH:SetExecutionCallback(HapticRightCB)
+BodyZones.lowerBackZoneRH:SetExecutionCallback(HapticRightCB)
